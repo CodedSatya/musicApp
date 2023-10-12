85ba09587940952f89e31a82319ec5daed53596d
Satyam Sekhar Barik <satyambarik574@gmail.com>
2023-10-12 20:58:58 +0530

    song data update

--- src/database/musics.js
+++ src/database/musics.js
@@ -4,8 +4,8 @@ import song2 from './files/demoSong2.mp3'
 export const musicList = [
   {
     "id": 1,
-    "title": "Habibi",
-    "artist": "xyz",
+    "title": "Bhool Jaa",
+    "artist": "Arijit Singh",
     "album": "",
     "year": "",
     "cover": "https://i.pinimg.com/564x/c4/bc/f4/c4bcf4c21d3e157a13f36188feb33271.jpg",
@@ -13,8 +13,8 @@ export const musicList = [
   },
   {
     "id": 2,
-    "title": "New HAbibi",
-    "artist": "abc",
+    "title": "Bhula Du",
+    "artist": " Stebin Ben ",
     "album": "",
     "year": "",
     "cover": "https://images.squarespace-cdn.com/content/56454c01e4b0177ad4141742/1458827328980-JNJHPFWPV4288CAJ8R5T/Covers-Vol.-1-Cover.jpg?content-type=image%2Fjpeg",
@@ -22,8 +22,8 @@ export const musicList = [
   },
   {
     "id": 3,
-    "title": "Habibi",
-    "artist": "xyz",
+    "title": "Bhool Jaa",
+    "artist": "Arijit Singh",
     "album": "",
     "year": "",
     "cover": "https://marketplace.canva.com/EAE1ApWPkQU/1/0/1600w/canva-memories-animated-music-album-cover-art-1Nr6gsUndKw.jpg",
